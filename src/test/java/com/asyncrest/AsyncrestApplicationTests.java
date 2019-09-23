558172904bc5f571d03ed0057bbdbe5dbc821176
Karol Mendza <karolmendza@gmail.com>
2019-09-23 22:27:24 +0200

    change to test module

--- src/test/java/com/asyncrest/AsyncrestApplicationTests.java
+++ src/test/java/com/asyncrest/AsyncrestApplicationTests.java
@@ -2,12 +2,29 @@ package com.asyncrest;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import reactor.core.publisher.ConnectableFlux;
+import reactor.core.publisher.Flux;
+
+import static java.time.Duration.ofSeconds;
 
 @SpringBootTest
 class AsyncrestApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void testFlux() {
+		ConnectableFlux<Object> publish = Flux.create(fluxSink -> {
+			while(true) {
+				fluxSink.next(System.currentTimeMillis());
+			}
+		})
+				.sample(ofSeconds(2))
+				.publish();
+
+		publish.subscribe();
 	}
 
 }
